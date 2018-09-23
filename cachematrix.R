0194ffb2dff638475a10f252dafdcd6dadaa33af
Elena <elena.yusupova@sd-group.com.au>
2018-09-23 15:54:30 +1000

    Ready functions

--- cachematrix.R
+++ cachematrix.R
@@ -6,12 +6,51 @@
 ## Test commit
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+}
+
+# Testing
+if (F) {
+    # Create inversable matrix
+    A <- matrix( c(5, 1, 0,
+                   3,-1, 2,
+                   4, 0,-1), nrow=3, byrow=TRUE)
+    # Expected output without cache
+    solve(A)
+    
+    # Create object with cache functionality
+    A.c <- makeCacheMatrix(A)
+    
+    # Creating cache
+    cacheSolve(A.c)
+    
+    # Using cache
+    cacheSolve(A.c)
 }
